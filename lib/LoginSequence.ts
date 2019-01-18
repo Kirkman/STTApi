cc745f16bb394ac5f3d46076edf62b2c7d83d4c7
Josh Renaud <josh@joshrenaud.com>
2019-01-18 11:21:57 -0600

    Set allcrew to empty array until we can figure out how to recreate the JSON file

--- lib/LoginSequence.ts
+++ lib/LoginSequence.ts
@@ -231,8 +231,11 @@ export async function loginSequence(onProgress: (description: string) => void, l
 
     onProgress('Loading crew cache...');
 
-    let allcrew = await STTApi.networkHelper.get(STTApi.serverAddress + 'allcrew.json', undefined);
-    STTApi.allcrew = formatAllCrew(allcrew);
+    //let allcrew = await STTApi.networkHelper.get(STTApi.serverAddress + 'allcrew.json', undefined);
+    //let allcrew = await STTApi.networkHelper.get('http://joshrenaud.com/experiments/star-trek-timelines/get_avatar_crew_archetypes.json', undefined);
+    let allcrew = [];
+    STTApi.allcrew = [];
+
 
     if (!STTApi.inWebMode) {
         onProgress('Loading equipment...');
